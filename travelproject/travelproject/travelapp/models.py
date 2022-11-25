270c17bc41b1272fc548da16c0a18260848af507
Khalidmahamood <khalidmahamood84@gmail.com>
2022-11-25 11:57:02 +0530

    added

---
+++ travelproject/travelproject/travelapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# class places(models.Model):
+#     name=models.CharField(max_length=200)
+#     image=models.ImageField(upload_to='pics')
+#     desx=models.TextField()
